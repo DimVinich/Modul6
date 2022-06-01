451697583efe3534450004811a12b27d154ab03d
DimVinich <DmitrijVS@gmail.com>
2022-06-01 20:09:14 +0300

    Задание 6.2.8

--- Modul6/Program.cs
+++ Modul6/Program.cs
@@ -7,10 +7,40 @@ namespace Modul6
 		static void Main(string[] args)
 		{
 
+			Animal animal = new Animal { type = "Собака", name = "Вольт", age = 4};
 
 			Console.ReadKey();
 
 		}
+
+		class Rectngle
+        {
+			public int a;
+			public int b;
+
+            Rectngle(int aa, int bb)
+            {
+				a = aa;
+				b = bb;
+            }
+
+            Rectngle(int aa)
+            {
+				a = aa;
+				b = aa;
+            }
+
+            Rectngle()
+            {
+				a = 6;
+				b = 4;
+            }
+
+            int Square()
+            {
+				return a * b;
+            }
+        }
 
 		class Pen
 		{
